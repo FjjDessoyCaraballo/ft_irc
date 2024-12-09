784e65ca7a004eda79784c3c2e2aa52acd1dd9a8
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-12-09 12:09:28 +0200

    feature: building parsing for input

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,23 @@
 #include <cstring>
 #include <unistd.h>
 
+/**
+ * TO DO 
+ *  
+ * 1. parse the port input to make sure we are receiving numerical 16 bits info;
+ * 2. Check weird inputs to htons();
+ * 3. Check with group about password policy;
+ * 4. encrypted port or no?
+ * 5. port range
+ */ 
 int main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
-		(void)argv;
+		std::string port = argv[1];
+		std::string password = argv[2];
+		if (password.empty() || port.empty())
+			return (1);
 		int socket_fd;
 
 		socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
